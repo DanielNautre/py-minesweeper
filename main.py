12524fb0403925aee080969a9cee0d6ff22414af
Daniel <daniel.nautre@gmail.com>
2016-04-26 11:22:06 +0200

    Implement hint creation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from random import randint
 
 
 sizex, sizey = 10, 10
-nbmines = 10
+nbMines = 10
 
 hints = [[0 for x in range(sizex)] for y in range(sizey)]
 field = [[0 for x in range(sizex)] for y in range(sizey)]
@@ -13,16 +13,52 @@ field = [[0 for x in range(sizex)] for y in range(sizey)]
 
 def fillMineField():
 
-    mineslefttoplace = nbmines
-    print mineslefttoplace
+    minesLeftToPlace = nbMines
 
-    while mineslefttoplace > 0:
+    while minesLeftToPlace > 0:
         randx, randy = randint(0, 9), randint(0, 9)
-        currentcell = field[randx][randy]
-        if currentcell == 1:
+        currentCell = field[randx][randy]
+        if currentCell == 1:
             pass
         else:
             field[randx][randy] = 1
-            mineslefttoplace = mineslefttoplace - 1
+            minesLeftToPlace = minesLeftToPlace - 1
+
+
+def fillHints():
+
+    for x in range(0, 10):
+        for y in range(0, 10):
+            if field[x][y] == 1:
+                hints[x][y] = 'X'
+            else:
+                mineCount = 0
+                if x > 0 and y > 0:
+                    mineCount = mineCount + field[x-1][y-1]
+                if x > 0:
+                    mineCount = mineCount + field[x-1][y]
+                if x > 0 and y < 9:
+                    mineCount = mineCount + field[x-1][y+1]
+                if y > 0:
+                    mineCount = mineCount + field[x][y-1]
+                if y < 9:
+                    mineCount = mineCount + field[x][y+1]
+                if x < 9 and y > 0:
+                    mineCount = mineCount + field[x+1][y-1]
+                if x < 9:
+                    mineCount = mineCount + field[x+1][y]
+                if x < 9 and y < 9:
+                    mineCount = mineCount + field[x+1][y+1]
+
+                hints[x][y] = mineCount
+
 
 fillMineField()
+fillHints()
+
+
+# Dirty way of displaying the matrices for debug
+
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in field]))
+print "\n\n"
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in hints]))
