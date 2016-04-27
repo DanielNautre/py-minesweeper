a7bbae431e81f3bb198102c7c14aa2abc3316943
Daniel <daniel.nautre@gmail.com>
2016-04-27 10:36:27 +0200

    Made the code more flexible to allow for different puzzle size

--- main.py
+++ main.py
@@ -7,70 +7,70 @@ from Tkinter import *
 WIDTH = 400
 HEIGHT = 400
 
-sizeRow, sizeCol = 10, 10
-nbMines = 10
+sizeY, sizeX = 10, 10
+nbMines = 20
 
-hints = [[0 for x in range(sizeRow)] for y in range(sizeCol)]
-field = [[0 for x in range(sizeRow)] for y in range(sizeCol)]
+hints = [[0 for x in range(sizeX)] for y in range(sizeY)]
+field = [[0 for x in range(sizeX)] for y in range(sizeY)]
 
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in field]))
+print "\n\n"
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in hints]))
 
 def fillMineField():
 
     minesLeftToPlace = nbMines
 
     while minesLeftToPlace > 0:
-        randRow, randCol = randint(0, 9), randint(0, 9)
-        currentCell = field[randRow][randCol]
+        randY, randX = randint(0, sizeY-1), randint(0, sizeX-1)
+        currentCell = field[randY][randX]
         if currentCell == 1:
             pass
         else:
-            field[randRow][randCol] = 1
+            field[randY][randX] = 1
             minesLeftToPlace = minesLeftToPlace - 1
 
 
 def fillHints():
 
-    for x in range(sizeRow):
-        for y in range(sizeCol):
-            if field[x][y] == 1:
-                hints[x][y] = 'X'
+    for y in range(sizeY):
+        for x in range(sizeX):
+            if field[y][x] == 1:
+                hints[y][x] = 'X'
             else:
                 mineCount = 0
                 if x > 0 and y > 0:
-                    mineCount = mineCount + field[x-1][y-1]
+                    mineCount = mineCount + field[y-1][x-1]
                 if x > 0:
-                    mineCount = mineCount + field[x-1][y]
-                if x > 0 and y < 9:
-                    mineCount = mineCount + field[x-1][y+1]
+                    mineCount = mineCount + field[y][x-1]
+                if x > 0 and y < (sizeY-2):
+                    mineCount = mineCount + field[y+1][x-1]
                 if y > 0:
-                    mineCount = mineCount + field[x][y-1]
-                if y < 9:
-                    mineCount = mineCount + field[x][y+1]
-                if x < 9 and y > 0:
-                    mineCount = mineCount + field[x+1][y-1]
-                if x < 9:
-                    mineCount = mineCount + field[x+1][y]
-                if x < 9 and y < 9:
-                    mineCount = mineCount + field[x+1][y+1]
+                    mineCount = mineCount + field[y-1][x]
+                if y < (sizeY-2):
+                    mineCount = mineCount + field[y+1][x]
+                if x < (sizeX-2) and y > 0:
+                    mineCount = mineCount + field[y-1][x+1]
+                if x < (sizeX-2):
+                    mineCount = mineCount + field[y][x+1]
+                if x < (sizeX-2) and y < (sizeY-2):
+                    mineCount = mineCount + field[y+1][x+1]
 
-                hints[x][y] = mineCount
+                hints[y][x] = mineCount
+
 
 def createPlayfield():
 
     playfield = Canvas(window, width=WIDTH, height=HEIGHT)
 
-    for x in range(sizeRow):
-        for y in range(sizeCol):
+    for x in range(sizeY):
+        for y in range(sizeX):
             playfield.create_text(10 + (30 * x), 10 + (30 * y), text=hints[x][y])
 
     playfield.pack()
 
 
-# Dirty way of displaying the matrices for debug
 
-print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in field]))
-print "\n\n"
-print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in hints]))
 
 
 window = Tk()
@@ -78,5 +78,11 @@ window = Tk()
 fillMineField()
 fillHints()
 createPlayfield()
+
+# Dirty way of displaying the matrices for debug
+
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in field]))
+print "\n\n"
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in hints]))
 
 window.mainloop()
