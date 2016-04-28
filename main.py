de75aef636c0ccd627a468e5ec974264dc3720fd
Daniel <daniel.nautre@gmail.com>
2016-04-28 17:02:11 +0200

    Change: Variable names to avoid confusion between canvas coords, and grid position

--- main.py
+++ main.py
@@ -21,70 +21,71 @@ class minesweeper(Tkinter.Tk):
 
     def createValues(self):
         self.CELLSIZE = 30
-        self.sizeY = 10 # height of the grid
-        self.sizeX = 10 # width of the grid
+        self.nbRow = 10 # height of the grid (Y): aka nb of Rows
+        self.nbCol = 10 # width of the grid (X): aka nb of Cols
         self.nbMines = 10
 
         # Defines the height and width of the window based upon 
         # the grid size and the cell size
-        self.WIDTH = (10 * 2) + (self.sizeY * self.CELLSIZE)
-        self.HEIGHT = (10 * 2) + (self.sizeX * self.CELLSIZE) 
+        self.WIDTH = (10 * 2) + (self.nbRow * self.CELLSIZE)
+        self.HEIGHT = (10 * 2) + (self.nbCol * self.CELLSIZE) 
 
         #creates the various 2d arrays that will be used in teh game
-        self.hints = [[0 for x in range(self.sizeX)] for y in range(self.sizeY)]
-        self.field = [[0 for x in range(self.sizeX)] for y in range(self.sizeY)]
-        self.cells = [[0 for x in range(self.sizeX)] for y in range(self.sizeY)]
+        self.hints = [[0 for col in range(self.nbCol)] for row in range(self.nbRow)]
+        self.field = [[0 for col in range(self.nbCol)] for row in range(self.nbRow)]
+        self.cells = [[0 for col in range(self.nbCol)] for row in range(self.nbRow)]
 
     def fillMineField(self):
 
         minesLeftToPlace = self.nbMines
 
         while minesLeftToPlace > 0:
-            randY, randX = randint(0, self.sizeY-1), randint(0, self.sizeX-1)
-            currentCell = self.field[randY][randX]
+            randRow = randint(0, self.nbRow-1)
+            randCol = randint(0, self.nbCol-1)
+            currentCell = self.field[randRow][randCol]
             if currentCell == 1:
                 pass
             else:
-                self.field[randY][randX] = 1
+                self.field[randRow][randCol] = 1
                 minesLeftToPlace = minesLeftToPlace - 1
 
 
     def fillHints(self):
         """ This function fills the hints array with the correct hint depending on the field array """
 
-        for y in range(self.sizeY):
-            for x in range(self.sizeX):
+        for row in range(self.nbRow):
+            for col in range(self.nbCol):
 
                 # if there is a mine, show a "X" symbol
-                if self.field[y][x] == 1:
-                    self.hints[y][x] = 'X'
+                if self.field[row][col] == 1:
+                    self.hints[row][col] = 'X'
                 else:
                     mineCount = 0
-                    if x > 0 and y > 0:
-                        mineCount = mineCount + self.field[y-1][x-1]
-                    if x > 0:
-                        mineCount = mineCount + self.field[y][x-1]
-                    if x > 0 and y < (self.sizeY-1):
-                        mineCount = mineCount + self.field[y+1][x-1]
-                    if y > 0:
-                        mineCount = mineCount + self.field[y-1][x]
-                    if y < (self.sizeY-1):
-                        mineCount = mineCount + self.field[y+1][x]
-                    if x < (self.sizeX-1) and y > 0:
-                        mineCount = mineCount + self.field[y-1][x+1]
-                    if x < (self.sizeX-1):
-                        mineCount = mineCount + self.field[y][x+1]
-                    if x < (self.sizeX-1) and y < (self.sizeY-1):
-                        mineCount = mineCount + self.field[y+1][x+1]
+                    if col > 0 and row > 0:
+                        mineCount = mineCount + self.field[row-1][col-1]
+                    if col > 0:
+                        mineCount = mineCount + self.field[row][col-1]
+                    if col > 0 and row < (self.nbRow-1):
+                        mineCount = mineCount + self.field[row+1][col-1]
+                    if row > 0:
+                        mineCount = mineCount + self.field[row-1][col]
+                    if row < (self.nbRow-1):
+                        mineCount = mineCount + self.field[row+1][col]
+                    if col < (self.nbCol-1) and row > 0:
+                        mineCount = mineCount + self.field[row-1][col+1]
+                    if col < (self.nbCol-1):
+                        mineCount = mineCount + self.field[row][col+1]
+                    if col < (self.nbCol-1) and row < (self.nbRow-1):
+                        mineCount = mineCount + self.field[row+1][col+1]
 
-                    self.hints[y][x] = mineCount
+                    self.hints[row][col] = mineCount
 
 
     def activateCell(self, event):
         cell = event.widget.find_closest(event.x, event.y)
         self.playfield.delete(cell)
         coords = self.getCoords(cell)
-        cellValue = self.hints[coords[1]][coords[0]]
+        cellValue = self.hints[coords[0]][coords[1]]
         if cellValue == "X":
             pass
             # Boum you're dead
@@ -108,12 +109,12 @@ class minesweeper(Tkinter.Tk):
         spacer = (self.CELLSIZE / 2)
         self.playfield = Tkinter.Canvas(self, width=self.WIDTH, height=self.HEIGHT)
 
-        for x in range(self.sizeY):
-            for y in range(self.sizeX):
+        for row in range(self.nbRow):
+            for col in range(self.nbCol):
                 
                 # start position for the cells
-                posX = 10 + (self.CELLSIZE * x)
-                posY = 10 + (self.CELLSIZE * y)
+                posX = 10 + (self.CELLSIZE * col)
+                posY = 10 + (self.CELLSIZE * row)
 
                 # end position for the cells
                 posXbis = posX + self.CELLSIZE
@@ -123,25 +124,25 @@ class minesweeper(Tkinter.Tk):
                 self.playfield.create_rectangle(posX, posY, posXbis, posYbis)
 
                 # position hints
-                self.playfield.create_text(posX + spacer, posY + spacer, text=self.hints[x][y])
+                self.playfield.create_text(posX + spacer, posY + spacer, text=self.hints[row][col])
                 
                 # position "buttons"
-                self.cells[y][x] = self.playfield.create_rectangle(posX, posY, posXbis, posYbis, fill="white")
+                self.cells[row][col] = self.playfield.create_rectangle(posX, posY, posXbis, posYbis, fill="white")
                 
 
                 # bind mouse clicks on the playfield
-                self.playfield.tag_bind(self.cells[y][x], '<ButtonPress-1>', self.activateCell)
-                self.playfield.tag_bind(self.cells[y][x], '<ButtonPress-3>', self.placeFlag)
+                self.playfield.tag_bind(self.cells[row][col], '<ButtonPress-1>', self.activateCell)
+                self.playfield.tag_bind(self.cells[row][col], '<ButtonPress-3>', self.placeFlag)
 
         self.playfield.pack()
 
 
     def getCoords(self, cell):
         """ Using the cell Tkinter id, find out it's X and Y value"""
-        for x in range(self.sizeY):
-            for y in range(self.sizeX):
-                if self.cells[y][x] == cell[0]:
-                    return [x, y]
+        for row in range(self.nbRow):
+            for col in range(self.nbCol):
+                if self.cells[row][col] == cell[0]:
+                    return [row, col]
 
 
 if __name__ == "__main__":
